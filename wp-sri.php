78796b2228a8ca66b4f3a52127cac3478645b664
Brennan Goewert <bgoewert@seilerinst.com>
2022-09-07 09:15:34 -0500

    dev: updated required versions of WP and PHP
    to align with tested versions

--- wp-sri.php
+++ wp-sri.php
@@ -4,7 +4,8 @@
  * Plugin URI: https://maymay.net/blog/projects/wp-sri/
  * Description: A utility to easily add SRI security checks to your generated WordPress pages. <strong>Like this plugin? Please <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&amp;business=TJLPJYXHSRBEE&amp;lc=US&amp;item_name=WordPress%20Subresource%20Integrity%20Plugin&amp;item_number=wp-sri&amp;currency_code=USD&amp;bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHosted" title="Send a donation to the maintainer">donate</a>. &hearts; Thank you!</strong>
  * Version: 0.5.0
- * Requires at least: 4.1
+ * Requires at least: 5.6
+ * Requires PHP: 7.4
  * Text Domain: wp-sri
  * Domain Path: /languages
  */
